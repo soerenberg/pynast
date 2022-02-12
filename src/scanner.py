b258a62e64bfdf2722249344cd8ca4095ce6208c
Soeren Berg <soeren.berg@protonmail.com>
2022-02-12 15:26:03 +0100

    Token data type

--- src/scanner.py
+++ src/scanner.py
@@ -1,5 +1,6 @@
 """Scan stan code."""
 from enum import Enum, auto
+from typing import NamedTuple
 
 
 class TokenType(Enum):
@@ -12,3 +13,20 @@ class TokenType(Enum):
     IDENTIFIER = auto()
 
     EOF = auto()
+
+
+class Token(NamedTuple):
+    """Token.
+
+    Attributes:
+        ttype: type of token
+        line: line the token was scanned on
+        column: horizontal position the token was scanned on
+        lexeme: string as the token was scanned
+        literal: literal value, only used for literals.
+    """
+    ttype: TokenType
+    line: int
+    column: int
+    lexeme: str = ""
+    literal: Any = None
