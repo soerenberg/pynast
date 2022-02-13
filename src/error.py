6c41d63da5622d125e03d7ff28aefbcfbca1b6a9
Soeren Berg <soeren.berg@protonmail.com>
2022-02-13 19:55:11 +0100

    error module, throw_at, throw_token methods

---
+++ src/error.py
@@ -0,0 +1,14 @@
+"""Function for reporting errors."""
+import scanner
+
+
+def throw_at(line: int, column: int, message: str):
+    """Report error to std out, raise RuntimeError."""
+    message = f"[line {line}, column {column}]: {message}"
+    print(message)
+    raise RuntimeError(message)
+
+
+def throw_token(token: scanner.Token, message: str) -> None:
+    """Report error to std out, raise RuntimeError."""
+    throw_at(token.line, token.column, message)
---
+++ src/tests/test_error.py
@@ -0,0 +1,28 @@
+"""Tests for error.py module."""
+import re
+
+import pytest
+
+import error
+import scanner
+
+
+@pytest.mark.parametrize("line,column,message,expected_msg", [
+    (2, 3, "lipsum", "[line 2, column 3]: lipsum"),
+    (12, 23, "lorem", "[line 12, column 23]: lorem"),
+])
+def test_throw_at(line, column, message, expected_msg):
+    """Test error.throw_at."""
+    with pytest.raises(RuntimeError, match=re.escape(expected_msg)):
+        error.throw_at(line, column, message)
+
+
+@pytest.mark.parametrize("token,message", [
+    (scanner.Token(scanner.TokenType.STRING, 2, 3, "", None), "lipsum"),
+    (scanner.Token(scanner.TokenType.SPACE, 12, 23, "", None), "lorem"),
+])
+def test_throw_token(token, message, mocker):
+    """Test error.throw_token."""
+    mocker.patch.object(error, "throw_at")
+    error.throw_token(token, message)
+    error.throw_at.assert_called_once_with(token.line, token.column, message)
