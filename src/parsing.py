966ae18127e8e1760e60606525c937369f671b32
Soeren Berg <soeren.berg@protonmail.com>
2022-02-26 14:32:28 +0100

    rudimentary parser

---
+++ src/parsing.py
@@ -0,0 +1,37 @@
+"""Stan parser."""
+from typing import Any, List
+from tokens import Token, TokenType
+
+
+class Parser:
+    """Stan parser."""
+
+    def __init__(self, token_list: List[Token]):
+        self._token_list = list(token_list)
+        self._current = 0
+
+    def parse(self) -> List[Any]:
+        """Run parser."""
+        result = []
+
+        while not self._is_at_end():
+            self._pop_token()
+            result.append(None)
+
+        return result
+
+    def _pop_token(self):
+        token = self._peek()
+
+        if not self._is_at_end():
+            self._current += 1
+
+        return token
+
+    def _is_at_end(self) -> bool:
+        """Check if EOF encountered yet."""
+        return self._peek().ttype == TokenType.EOF
+
+    def _peek(self) -> Token:
+        """Peek at current element."""
+        return self._token_list[self._current]
