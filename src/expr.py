d15b3e248aa45f2b5c3f00855520bec7d31d5ff9
Soeren Berg <soeren.berg@protonmail.com>
2022-03-07 17:12:51 +0100

    Unary expression

--- src/expr.py
+++ src/expr.py
@@ -4,6 +4,8 @@ from __future__ import annotations
 import abc
 from typing import Any
 
+import tokens
+
 
 class Expr:
     """Base class for expressions."""
@@ -15,5 +17,23 @@ class Expr:
         """Accept method for the visitor pattern."""
 
 
+class Unary(Expr):
+    """Unary operation with one operator and one expression."""
+
+    # pylint: disable=too-few-public-methods
+
+    def __init__(self, operator: tokens.Token, right: Expr):
+        super().__init__()
+        self.operator = operator
+        self.right = right
+
+    def accept(self, visitor: Visitor) -> Any:
+        return visitor.visit_unary(self)
+
+
 class Visitor:
     """Visitor for Expr types."""
+
+    @abc.abstractmethod
+    def visit_unary(self, expr: Unary) -> Any:
+        """Visit Unary."""
