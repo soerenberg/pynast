6340a27a3da8ca1b468ef3b85986190461131e45
Soeren Berg <soeren.berg@protonmail.com>
2022-05-11 23:36:53 +0200

    stmt.Declaration

--- src/stmt.py
+++ src/stmt.py
@@ -1,7 +1,10 @@
 """Statement nodes for ASTs."""
 from __future__ import annotations
 import abc
-from typing import Any
+from typing import Any, Optional
+
+import expr
+import tokens
 
 
 class Stmt:
@@ -14,5 +17,45 @@ class Stmt:
         """Accept method for the visitor pattern."""
 
 
+class Declaration(Stmt):
+    """Variable declaration statement."""
+
+    # pylint: disable=too-few-public-methods
+
+    def __init__(self,
+                 ttype: tokens.Token,
+                 identifier: tokens.Token,
+                 lower: Optional[expr.Expr] = None,
+                 upper: Optional[expr.Expr] = None,
+                 offset: Optional[expr.Expr] = None,
+                 multiplier: Optional[expr.Expr] = None,
+                 initializer: Optional[expr.Expr] = None):
+        # pylint: disable=too-many-arguments
+        self.ttype = ttype
+        self.identifier = identifier
+        self.lower = lower
+        self.upper = upper
+        self.offset = offset
+        self.multiplier = multiplier
+        self.initializer = initializer
+
+    def accept(self, visitor: Visitor) -> Any:
+        return visitor.visit_declaration(self)
+
+    def __eq__(self, other):
+        return (isinstance(other, Declaration) and self.ttype == other.ttype
+                and self.identifier == other.identifier
+                and self.lower == other.lower and self.upper == other.upper
+                and self.offset == other.offset
+                and self.multiplier == other.multiplier
+                and self.initializer == other.initializer)
+
+
 class Visitor:
     """Visitor for Stmt types."""
+
+    # pylint: disable=too-few-public-methods
+
+    @abc.abstractmethod
+    def visit_declaration(self, statement: Declaration) -> Any:
+        """Visit Declaration."""
